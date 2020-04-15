435ba49b9e5f9b8b4e76291cff0e2661466a891f
Konstantinos Tzoulas <konsta@sin.ga>
2020-04-15 12:19:45 +0300

    meta things

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,9 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'og:image', property: 'og:image', content: '/logo.png' },
+      { hid: 'image', property: 'image', content: '/logo.png' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
