443c1661a4dcfd87efa476efb0955b85baabcca3
Konstantinos Tzoulas <konsta@singa.com>
2023-06-11 12:49:33 +0300

    babyy

--- components/LappuBaby.vue
+++ components/LappuBaby.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 	.lappu-wrap
 		.header
-			img.logo(src="/baby.PNG")
+			img.logo(src="/baby.png")
 		.ruudut(v-if="currentList")
 			button.ruutu(v-for="i in currentList.songs" :key="i.artist" @click="artistClick(i)" :class="{'selected': i.selected}") 
 				span {{i.artist}}
