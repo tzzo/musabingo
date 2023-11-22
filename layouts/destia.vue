17c05e8a5e5cd06dfeb3bda98e54009e46ee0bb9
Konstantinos Tzoulas <konsta@singa.com>
2023-11-22 20:49:27 +0200

    destia

---
+++ layouts/destia.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style lang="sass">
+html
+  font-size: 16px
+  word-spacing: 1px
+  -ms-text-size-adjust: 100%
+  -webkit-text-size-adjust: 100%
+  -moz-osx-font-smoothing: grayscale
+  -webkit-font-smoothing: antialiased
+  box-sizing: border-box
+  background: url("/bingotausta.png"), rgba(black,.5)
+  background-position: center
+  background-size: cover
+  font-family: 'Contrail One', cursive
+
+*,
+*:before,
+*:after
+  box-sizing: border-box
+  margin: 0
+</style>
